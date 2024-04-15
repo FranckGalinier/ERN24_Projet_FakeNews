92feb2152584f25f755c529d5f0cda65e916a24a
Franck Galinier <fgalinier51@gmail.com>
2024-04-15 22:53:48 +0200

    import des polices et des instructions dans le code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('chargé');
+
+// Menu (en mode hamburger): s’ouvre et se ferme au clic sur le bouton.
+// Validation du formulaire:
+// Au clic sur le bouton, contrôler la saisie de l’utilisateur selon les règles:
+// - Nom: Pas vide, pas de chiffres, pas de caractères spéciaux sauf tiret
+// - Email: Doit avoir la forme X@X.X (X = un ou plusieurs caractères)
+// - Message: Pas vide
+// Si la validation échoue les champs erronés sont vidés et leur message d’erreur 
+// s’affiche. Si l’utilisateur place le curseur sur un champ erroné, l’erreur disparaît.
+// Si la validation réussit la modale de succès s’affiche (on la ferme au clic sur son 
+//bouton
